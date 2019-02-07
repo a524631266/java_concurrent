c250cae8b602c317c5b49bcc7d22435d3aedb7b3
zhangll <524631266@qq.com>
2019-02-07 16:29:41 +0800

    饿汉式(单例)最基本的线程安全

---
+++ concurrent/src/main/java/zhangll/learningplayground/level2/cap1singleton/background/SingleTonHungary.java
@@ -0,0 +1,16 @@
+package zhangll.learningplayground.level2.cap1singleton.background;
+
+import java.util.Arrays;
+
+public class SingleTonHungary {
+    public static Object foo = new Object();
+
+    public static void main(String[] args) {
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8).stream().forEach(i -> new Thread(i + "") {
+            @Override
+            public void run() {
+                System.out.println("i:[" + i + "]" + Thread.currentThread().getName() + "Hungray:" + foo.hashCode());
+            }
+        }.start());
+    }
+}
