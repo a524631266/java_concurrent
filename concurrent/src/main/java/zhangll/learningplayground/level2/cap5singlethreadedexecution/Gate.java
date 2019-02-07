2c6a9c860750a2354d91749c7d59e443ad884b02
zhangll <524631266@qq.com>
2019-02-08 00:59:55 +0800

    利用读写锁控制之前的cap5案例，效果提升5-8毫秒左右

--- concurrent/src/main/java/zhangll/learningplayground/level2/cap5singlethreadedexecution/Gate.java
+++ concurrent/src/main/java/zhangll/learningplayground/level2/cap5singlethreadedexecution/Gate.java
@@ -1,15 +1,36 @@
 package zhangll.learningplayground.level2.cap5singlethreadedexecution;
 
+import zhangll.learningplayground.level2.cap6readwritelock.ReadWriteLock;
+
 public class Gate {
     private int count = 0;
     private String name;
     private String Address;
+    private static ReadWriteLock LOCK = new ReadWriteLock();
+    private long starttime = System.currentTimeMillis();
 
-    public synchronized void pass(String name, String Address) {
-        this.count++;
-        this.name = name;
-        this.Address = Address;
-        vertify();
+    // public synchronized void pass(String name, String Address) {
+    // this.count++;
+    // this.name = name;
+    // this.Address = Address;
+    // vertify();
+    // System.out.println("释放锁" + (System.currentTimeMillis() - starttime));
+    // }
+    public void pass(String name, String Address) {
+        try {
+            LOCK.writelock();
+            this.count++;
+            this.name = name;
+            this.Address = Address;
+            vertify();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            LOCK.writeunlock();
+            System.out.println("释放锁" + (System.currentTimeMillis() - starttime));
+        }
+
     }
 
     private void vertify() {
